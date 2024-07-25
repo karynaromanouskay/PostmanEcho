56d90e7b6548c9506a289458ccc2674a57970244
karina <karynaromanouskaya@gmail.com>
2024-07-25 10:39:29 +0300

    src/test/java/ru/netology/aqa/PostmanEchoTest.java

---
+++ src/test/java/ru/netology/aqa/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.aqa;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+    @Test
+    void returnSendData() {
+
+        String text = "Hi";
+        given()
+                .baseUri("http://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("some data"))
+        ;
+
+    }
+}
